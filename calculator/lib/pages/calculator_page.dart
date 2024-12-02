68bf3abf157c84a9b435227be1abe51f2fc98489
Gabriel Schultz <gabrielschultz2807@gmail.com>
2024-12-02 16:15:12 -0300

    Add icon and remove coments

--- calculator/lib/pages/calculator_page.dart
+++ calculator/lib/pages/calculator_page.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: library_private_types_in_public_api
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
 
@@ -10,98 +11,102 @@ class CalculatorPage extends StatefulWidget {
 
 class _CalculatorPageState extends State<CalculatorPage> {
   String _output = "0";
-  String _currentOperator = "";  // Variável para armazenar a operação selecionada
-  String _previousValue = "";  // Armazena o valor anterior
+  String _currentOperator =
+      ""; // Variável para armazenar a operação selecionada
+  String _previousValue = ""; // Armazena o valor anterior
 
-void _buttonPressed(String buttonText) {
-  setState(() {
-    if (buttonText == "=") {
-      try {
-        // Avaliar a expressão com base no operador e nos valores armazenados
-        _output = _evaluateExpression("$_previousValue$_currentOperator$_output");
-        _currentOperator = "";  // Limpa o operador após o cálculo
-        _previousValue = "";    // Limpa o valor anterior após o cálculo
-      } catch (e) {
-        _output = "Erro";
-      }
-    } else if (buttonText == "+/-") {
-      if (_output != "0") {
-        if (_output.startsWith("-")) {
-          _output = _output.substring(1); // Remove o sinal de negativo
+  void _buttonPressed(String buttonText) {
+    setState(() {
+      if (buttonText == "=") {
+        try {
+          _output =
+              _evaluateExpression("$_previousValue$_currentOperator$_output");
+          _currentOperator = ""; // Limpa o operador após o cálculo
+          _previousValue = ""; // Limpa o valor anterior após o cálculo
+        } catch (e) {
+          _output = "Erro";
+        }
+      } else if (buttonText == "+/-") {
+        if (_output != "0") {
+          if (_output.startsWith("-")) {
+            _output = _output.substring(1); // Remove o sinal de negativo
+          } else {
+            _output = "-$_output"; // Adiciona o sinal de negativo
+          }
+        }
+      } else if (buttonText == "%") {
+        try {
+          double value = double.parse(_output.replaceAll(',', '.')) / 100;
+          _output = value.toString().replaceAll('.', ',');
+        } catch (e) {
+          _output = "Erro";
+        }
+      } else if (buttonText == "AC") {
+        _output = "0"; // Reseta a tela
+        _currentOperator = ""; // Limpa o operador selecionado
+        _previousValue = ""; // Limpa o valor anterior
+      } else if (buttonText == "C") {
+        _output =
+            _output.length > 1 ? _output.substring(0, _output.length - 1) : "0";
+      } else if (buttonText == "+" ||
+          buttonText == "-" ||
+          buttonText == "×" ||
+          buttonText == "÷") {
+        // Se for um operador, realiza o cálculo com o valor anterior, se já houver valor armazenado
+        if (_previousValue.isNotEmpty) {
+          _output =
+              _evaluateExpression("$_previousValue$_currentOperator$_output");
         } else {
-          _output = "-$_output"; // Adiciona o sinal de negativo
+          _previousValue =
+              _output; // Armazena o valor atual antes de pressionar o operador
+          _output = "0"; // Limpa a tela para o próximo número
+        }
+        _currentOperator = buttonText; // Armazena o operador selecionado
+      } else {
+        if (_output == "0") {
+          _output = buttonText; // Substitui o "0" inicial
+        } else {
+          _output += buttonText; // Adiciona o botão pressionado
         }
       }
-    } else if (buttonText == "%") {
-      try {
-        double value = double.parse(_output.replaceAll(',', '.')) / 100;
-        _output = value.toString().replaceAll('.', ',');
-      } catch (e) {
-        _output = "Erro";
-      }
-    } else if (buttonText == "AC") {
-      _output = "0";  // Reseta a tela
-      _currentOperator = "";  // Limpa o operador selecionado
-      _previousValue = "";    // Limpa o valor anterior
-    } else if (buttonText == "C") {
-      _output = _output.length > 1 ? _output.substring(0, _output.length - 1) : "0";
-    } else if (buttonText == "+" || buttonText == "-" || buttonText == "×" || buttonText == "÷") {
-      // Se for um operador, realiza o cálculo com o valor anterior, se já houver valor armazenado
-      if (_previousValue.isNotEmpty) {
-        _output = _evaluateExpression("$_previousValue$_currentOperator$_output");
-      } else {
-        _previousValue = _output;  // Armazena o valor atual antes de pressionar o operador
-        _output = "0";  // Limpa a tela para o próximo número
-      }
-      _currentOperator = buttonText;  // Armazena o operador selecionado
-    } else {
-      if (_output == "0") {
-        _output = buttonText;  // Substitui o "0" inicial
-      } else {
-        _output += buttonText;  // Adiciona o botão pressionado
-      }
+    });
+  }
+
+  String _evaluateExpression(String expression) {
+    try {
+      expression = expression.replaceAll('×', '*').replaceAll('÷', '/');
+      Parser p = Parser();
+      Expression exp = p.parse(expression);
+      ContextModel cm = ContextModel();
+      double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+      // Retorna o resultado como inteiro ou decimal
+      return eval == eval.toInt() ? eval.toInt().toString() : eval.toString();
+    } catch (e) {
+      return "Erro";
     }
-  });
-}
-
-
-String _evaluateExpression(String expression) {
-  try {
-    // Substitui as operações de "×" e "÷"
-    expression = expression.replaceAll('×', '*').replaceAll('÷', '/');
-    Parser p = Parser();
-    Expression exp = p.parse(expression);
-    ContextModel cm = ContextModel();
-    double eval = exp.evaluate(EvaluationType.REAL, cm);
-
-    // Retorna o resultado como inteiro ou decimal
-    return eval == eval.toInt() ? eval.toInt().toString() : eval.toString();
-  } catch (e) {
-    return "Erro";
   }
-}
 
-
-  // Função para determinar a cor do botão
-// Função para determinar a cor do botão
-Color _getButtonColor(String buttonText) {
-  if (buttonText == _currentOperator) {
-    return Colors.white; // Cor azul para o operador selecionado
-  } else if (buttonText == '=' || buttonText == '+' || buttonText == '-' || 
-             buttonText == '×' || buttonText == '÷') {
-    return Colors.orange; // Cor laranja para os operadores
-  } else if (buttonText == 'AC' || buttonText == '+/-' || buttonText == '%') {
-    return Color.fromARGB(255, 165, 165, 165); // Cor RGB(165, 165, 165)
-  } else {
-    return Colors.grey[800]!; // Cor cinza escura para os outros botões
+  Color _getButtonColor(String buttonText) {
+    if (buttonText == _currentOperator) {
+      return Colors.white;
+    } else if (buttonText == '=' ||
+        buttonText == '+' ||
+        buttonText == '-' ||
+        buttonText == '×' ||
+        buttonText == '÷') {
+      return Colors.orange; // Cor laranja para os operadores
+    } else if (buttonText == 'AC' || buttonText == '+/-' || buttonText == '%') {
+      return const Color.fromARGB(255, 165, 165, 165);
+    } else {
+      return Colors.grey[800]!; // Cor cinza escura para os outros botões
+    }
   }
-}
-
 
   // Função para determinar a cor do texto do botão
   Color _getTextColor(String buttonText) {
     if (buttonText == _currentOperator) {
-    return Colors.orange;
+      return Colors.orange;
     } else if (buttonText == 'AC' || buttonText == '+/-' || buttonText == '%') {
       return Colors.black; // Cor preta para AC, +/- e %
     } else {
@@ -143,11 +148,25 @@ Color _getButtonColor(String buttonText) {
             itemCount: 19, // Total de botões
             itemBuilder: (context, index) {
               List<String> buttons = [
-                'AC', '+/-', '%', '÷',
-                '7', '8', '9', '×',
-                '4', '5', '6', '-',
-                '1', '2', '3', '+',
-                '0', ',', '=', // Linha final com botão "0", vírgula e igual
+                'AC',
+                '+/-',
+                '%',
+                '÷',
+                '7',
+                '8',
+                '9',
+                '×',
+                '4',
+                '5',
+                '6',
+                '-',
+                '1',
+                '2',
+                '3',
+                '+',
+                '0',
+                ',',
+                '=',
               ];
 
               String buttonText = buttons[index];
@@ -160,6 +179,7 @@ Color _getButtonColor(String buttonText) {
                     color: _getButtonColor(buttonText),
                     borderRadius: BorderRadius.circular(100),
                   ),
+                  width: MediaQuery.of(context).size.width * 0.4,
                   child: TextButton(
                     onPressed: () => _buttonPressed(buttonText),
                     child: Text(
@@ -170,7 +190,6 @@ Color _getButtonColor(String buttonText) {
                       ),
                     ),
                   ),
-                  width: MediaQuery.of(context).size.width * 0.4, // Ocupa duas colunas
                 );
               }
 
--- calculator/lib/widgets/calculator_button.dart
+++ calculator/lib/widgets/calculator_button.dart
@@ -3,18 +3,18 @@ import 'package:flutter/material.dart';
 class CalculatorButton extends StatelessWidget {
   final String buttonText;
   final VoidCallback onPressed;
-  final int widthFactor; // Novo parâmetro para controlar a largura
+  final int widthFactor;
 
   const CalculatorButton({
     super.key,
     required this.buttonText,
     required this.onPressed,
-    this.widthFactor = 1, // Largura padrão de 1
+    this.widthFactor = 1,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: widthFactor == 2 ? 160 : 80, // Ajusta a largura do botão
       child: ElevatedButton(
         onPressed: onPressed,
